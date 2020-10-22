937930e1930243deb4f626e6358fcf140ea7153c
samliu1234 <64169932+samliu1234@users.noreply.github.com>
2020-10-22 16:13:57 -0400

    Does this work?

---
+++ StAugustineTestApp/StAugustineTestApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  StAugustineTestApp
+//
+//  Created by Sam Liu on 2020-10-22.
+//  Copyright © 2020 Sam Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
