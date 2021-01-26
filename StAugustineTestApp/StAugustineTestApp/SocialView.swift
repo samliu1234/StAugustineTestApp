767bb64d1a4e4cdaafe30898a281fa434182ca21
samliu1234 <64169932+samliu1234@users.noreply.github.com>
2021-01-25 19:50:24 -0500

    started socials

--- StAugustineTestApp/StAugustineTestApp/SocialView.swift
+++ StAugustineTestApp/StAugustineTestApp/SocialView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct SocialView: View {
+    
+    let pColor:Color = Color(red: 141/255, green: 18/255, blue: 48/255)
+    
     var body: some View {
-        Text("Social")
+        ScrollView {
+            
+            GeometryReader { geometry in
+            
+                VStack(alignment: .leading, spacing: 30) {
+                    
+                    // MARK: Title
+                    HStack {
+                    
+                        Text("Socials")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.white)
+                            .fixedSize(horizontal: false, vertical: true)
+                        
+                        Spacer()
+                        
+                    }
+                    .padding(.all, 25)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(pColor, lineWidth: 8)
+                    )
+                    .background(pColor)
+                    .cornerRadius(20)
+                    .padding([.horizontal, .top], 25)
+                    
+                    // MARK: Main body
+                    VStack() {
+                        
+                        Text("My clubs")
+                            .foregroundColor(pColor)
+                            .bold()
+                        
+                        
+                        Image(systemName: "heart.fill")
+
+                        
+                    }
+                    // Properties for song requests
+                    .frame(width: 0.7 * geometry.size.width, alignment: .center)
+                    .padding(.all, 25)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color(red: 0/255, green: 0/255, blue: 0/255, opacity: 0.15), lineWidth: 4))
+                    .padding(.all, 25)
+                    
+                }
+            }
+        }
     }
 }
 
