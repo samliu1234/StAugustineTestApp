d4c8fd04cea8a7c869d910ec8134df2aa2dda7ab
matthewm588 <64167590+matthewm588@users.noreply.github.com>
2021-01-31 13:57:40 -0500

    started on a grid view for caf menu

--- StAugustineTestApp/StAugustineTestApp/CafeMenuView.swift
+++ StAugustineTestApp/StAugustineTestApp/CafeMenuView.swift
@@ -5,11 +5,49 @@
 //  Created by Matthew Morelli on 2020-10-25.
 //
 
+import Foundation
 import SwiftUI
 
-struct CafeMenuView: View {
+struct ImageOverlay: View {
     var body: some View {
-        Text("Cafe Menu")
+        HStack {
+            Text("burger")
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct CafeMenuView: View {
+    
+    let data = Array(1...6).map {"Item: \($0)"}
+    let layout = [
+        GridItem(.flexible(maximum: 100)),
+        GridItem(.flexible(maximum: 100))
+    ]
+    let pColor:Color = Color(red: 141/255, green: 18/255, blue: 48/255)
+    
+    var body: some View {
+        
+        ScrollView {
+            LazyVGrid(columns: layout, spacing: 20) {
+                ForEach(data, id: \.self) {item in
+                    VStack {
+                        Image("burger")
+                            .resizable()
+                            .scaledToFit()
+                            .overlay(
+                                Text("burger").foregroundColor(.white), alignment: .bottomTrailing)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(pColor, lineWidth: 0.75))
+                    }
+                    .padding(.all)
+                    //.frame(width: 150, height: 350)
+                }
+            }
+            .padding(.horizontal)
+        }
+        
     }
 }
 
